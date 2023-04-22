ae9e432c2a4fde7300d8742634adb6134045ca99
doke93 <dokeabhishek3@gmail.com>
2023-04-22 11:56:02 +0530

    added doc to the random function

--- geoindia/geoindia.py
+++ geoindia/geoindia.py
@@ -2,11 +2,28 @@
 import random
 import string
 
-def generate_random_string(length):
+def generate_random_string(length=10):
+    """Generate a randowm string of a given length
+
+    Args:
+        length (int, optional): The length of the string to generate. Default to 10
+
+    Returns:
+        str: The generated string
+    """    
+
     characters = string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation
     rnd = random.SystemRandom()
     return ''.join(rnd.choice(characters) for i in range(length))
 
 def generate_lucky_number(length=1):
+    """Generate a randowm number of a given length
+
+    Args:
+        length (int, optional): The length of the string to generate. Default to 1.
+
+    Returns:
+        str: The generated number string
+    """
     result_str = ''.join(random.choice(string.digits) for i in range(length))
     return result_str
