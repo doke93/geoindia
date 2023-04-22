ea15ee9bfaf84258bf84e4906f242e03df591d13
doke93 <dokeabhishek3@gmail.com>
2023-04-22 10:58:50 +0530

    Added module

--- geoindia/geoindia.py
+++ geoindia/geoindia.py
@@ -1,1 +1,8 @@
 """Main module."""
+import random
+import string
+
+def generate_random_string(length):
+    characters = string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation
+    rnd = random.SystemRandom()
+    return ''.join(rnd.choice(characters) for i in range(length))
